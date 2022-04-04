5ffe0e14854ef5309bed021301d2fb2722d8f782
sworupsubedi <sworupsubedi2002@gmail.com>
2022-04-04 10:31:42 -0500

    test again

--- app/src/main/java/ApiInterface.kt
+++ app/src/main/java/ApiInterface.kt
@@ -1,2 +1,9 @@
-class ApiInterface {
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiInterface {
+    @POST("https://api-auth.prod.birdapp.com/api/v1/auth/email")
+
+
 }
